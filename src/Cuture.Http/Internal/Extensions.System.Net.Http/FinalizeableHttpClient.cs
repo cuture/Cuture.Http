95f8703ad2fc8a77d8491c40e110ea1556cce363
Stratos <StratosBlue@outlook.com>
2021-04-14 11:28:05 +0800

    现在内部使用的Httpclient不会请求超时

--- src/Cuture.Http/Internal/Extensions.System.Net.Http/FinalizeableHttpClient.cs
+++ src/Cuture.Http/Internal/Extensions.System.Net.Http/FinalizeableHttpClient.cs
@@ -6,6 +6,8 @@ namespace Cuture.Http
     /// 重写析构函数的<see cref="HttpClient"/>
     /// <para/>
     /// 在析构时将会调用 <see cref="HttpClient.Dispose(bool)"/> ，参数值为 true
+    /// <para/>
+    /// 请求超时为永不超时 <see cref="System.Threading.Timeout.InfiniteTimeSpan"/>
     /// </summary>
     internal sealed class FinalizeableHttpClient : HttpClient
     {
@@ -14,16 +16,19 @@ namespace Cuture.Http
         /// <inheritdoc/>
         public FinalizeableHttpClient()
         {
+            Timeout = System.Threading.Timeout.InfiniteTimeSpan;
         }
 
         /// <inheritdoc/>
         public FinalizeableHttpClient(HttpMessageHandler handler) : base(handler)
         {
+            Timeout = System.Threading.Timeout.InfiniteTimeSpan;
         }
 
         /// <inheritdoc/>
         public FinalizeableHttpClient(HttpMessageHandler handler, bool disposeHandler) : base(handler, disposeHandler)
         {
+            Timeout = System.Threading.Timeout.InfiniteTimeSpan;
         }
 
         #endregion Public 构造函数
