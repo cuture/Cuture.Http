8accb7c41a52838f95371a423a8357227ad099be
Stratos <StratosBlue@outlook.com>
2021-06-14 00:06:19 +0800

    修正一个单元测试不能工作的问题。。。

--- tests/Cuture.Http.Test/Internal/SpanExtensionsTest.cs
+++ tests/Cuture.Http.Test/Internal/SpanExtensionsTest.cs
@@ -1,5 +1,7 @@
 ﻿#if NETCOREAPP
+
 using System;
+using System.Linq;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -8,20 +10,22 @@ namespace Cuture.Http.Test.Internal
     [TestClass]
     public class SpanExtensionsTest
     {
+        #region Public 方法
+
         [TestMethod]
-        [DataRow("0123456789", '4', "0123", "56789")]
-        [DataRow("0123456789", '0', "", "123456789")]
-        [DataRow("0123456789", '9', "012345678", "")]
-        [DataRow("0123456789", '-', "0123456789", "0123456789")]
-        [DataRow("01234567890123456789", '4', "0123", "567890123456789")]
-        public void TruncationOneElementStartTest(string source, char indentifier, string targetFront, string targetBack)
+        [DataRow("0123456789", "456", "0123", "789")]
+        [DataRow("0123456789", "012", "", "3456789")]
+        [DataRow("0123456789", "789", "0123456", "")]
+        [DataRow("0123456789", "987", "0123456789", "0123456789")]
+        [DataRow("01234567890123456789", "456", "01234567890123", "789")]
+        public void TruncationElementsEndTest(string source, string indentifier, string targetFront, string targetBack)
         {
             var sourceSpan = source.AsSpan();
-            var @default = new string(sourceSpan.TruncationStart(indentifier));
+            var @default = new string(sourceSpan.TruncationEnd(indentifier));
             Assert.AreEqual(targetBack, @default);
-            var back = new string(sourceSpan.TruncationStart(indentifier, true));
+            var back = new string(sourceSpan.TruncationEnd(indentifier, true));
             Assert.AreEqual(targetBack, back);
-            var front = new string(sourceSpan.TruncationStart(indentifier, false));
+            var front = new string(sourceSpan.TruncationEnd(indentifier, false));
             Assert.AreEqual(targetFront, front);
         }
 
@@ -48,8 +52,10 @@ namespace Cuture.Http.Test.Internal
         [DataRow("0123456789", '9', "012345678", "")]
         [DataRow("0123456789", '-', "0123456789", "0123456789")]
         [DataRow("01234567890123456789", '4', "01234567890123", "56789")]
-        public void TruncationOneElementEndTest(string source, char indentifier, string targetFront, string targetBack)
+        public void TruncationOneElementEndTest(string source, string indentifierString, string targetFront, string targetBack)
         {
+            //测试不允许传入char参数？临时解决方案
+            var indentifier = indentifierString.First();
             var sourceSpan = source.AsSpan();
             var @default = new string(sourceSpan.TruncationEnd(indentifier));
             Assert.AreEqual(targetBack, @default);
@@ -60,21 +66,25 @@ namespace Cuture.Http.Test.Internal
         }
 
         [TestMethod]
-        [DataRow("0123456789", "456", "0123", "789")]
-        [DataRow("0123456789", "012", "", "3456789")]
-        [DataRow("0123456789", "789", "0123456", "")]
-        [DataRow("0123456789", "987", "0123456789", "0123456789")]
-        [DataRow("01234567890123456789", "456", "01234567890123", "789")]
-        public void TruncationElementsEndTest(string source, string indentifier, string targetFront, string targetBack)
+        [DataRow("0123456789", '4', "0123", "56789")]
+        [DataRow("0123456789", '0', "", "123456789")]
+        [DataRow("0123456789", '9', "012345678", "")]
+        [DataRow("0123456789", '-', "0123456789", "0123456789")]
+        [DataRow("01234567890123456789", '4', "0123", "567890123456789")]
+        public void TruncationOneElementStartTest(string source, string indentifierString, string targetFront, string targetBack)
         {
+            //测试不允许传入char参数？临时解决方案
+            var indentifier = indentifierString.First();
             var sourceSpan = source.AsSpan();
-            var @default = new string(sourceSpan.TruncationEnd(indentifier));
+            var @default = new string(sourceSpan.TruncationStart(indentifier));
             Assert.AreEqual(targetBack, @default);
-            var back = new string(sourceSpan.TruncationEnd(indentifier, true));
+            var back = new string(sourceSpan.TruncationStart(indentifier, true));
             Assert.AreEqual(targetBack, back);
-            var front = new string(sourceSpan.TruncationEnd(indentifier, false));
+            var front = new string(sourceSpan.TruncationStart(indentifier, false));
             Assert.AreEqual(targetFront, front);
         }
+
+        #endregion Public 方法
     }
 }
 
